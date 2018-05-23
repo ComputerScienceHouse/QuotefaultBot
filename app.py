705966f3d0c1faa5522a8233fa7eba263b11dfc4
Max Meinhold <mxmeinhold@gmail.com>
2018-05-23 11:18:47 -0400

    Sets up config

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
+
+if os.path.exists(os.path.join(os.getcwd(), "config.py")):
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
+else:
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
 
 @app.route('/')
 def index():
 	return "Hello World!"
 
+@app.route('/quote', methods=['POST'])
+def get_quote():
+	"""
+	Replies with a random quote.
+	:return: The reply message
+	"""
+	# TODO Parse the message from slack.
+	app.logger.info(request.form)
+
+
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(host = app.config['IP'], port = int(app.config['PORT']), debug = True)
---
+++ config.env.py
@@ -0,0 +1,16 @@
+import os
+
+# Flask Config
+DEBUG = True
+IP = os.environ.get('API_IP', '127.0.0.1')
+PORT = os.environ.get('API_PORT', 8080)
+SERVER_NAME = os.environ.get('API_SERVER_NAME', 'quotefault-bot.csh.rit.edu')
+
+# Quotefault API config
+QUOUTEFAULT_ADDR = os.environ.get('QUOTEFAULT_API_ADDR', 'quotefault-api.csh.rit.edu')
+QUOTEFAULT_KEY = os.environ.get('QUOTEFAULT_API_KEY', '')
+
+# Slack creds
+CLIENT_ID = os.environ.get('SLACK_CLIENT_ID', '');
+CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET', '')
+VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN', '')
