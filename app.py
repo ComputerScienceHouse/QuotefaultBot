7d952c2d6192777f6f02403efd30a561d194c793
Max Meinhold <mxmeinhold@gmail.com>
2018-05-24 11:01:41 -0400

    Implements date and submitter queries for random

--- app.py
+++ app.py
@@ -20,19 +20,18 @@ def get_quote():
 	Replies with a random quote.
 	:return: The reply message
 	"""
-	app.logger.info(request.form)
+	app.logger.info(request.form) # Debug
 	if(app.config['VERIFICATION_TOKEN'] == request.form['token']):
 		command = request.form['text'].split(' ')[0]
 		#TODO use a dictionary to map to functions in responses.py
 		if(command == 'help'):
 			return responses.help_msg('')
 		elif(command == 'random'):
-			return responses.random();
+			return responses.random(request.form['text']);
 		else:
 			return responses.help_msg(command)
 	else:
 		abort(401)
-
 
 if __name__ == '__main__':
 	app.run(host = app.config['IP'], port = int(app.config['PORT']), debug = True)
--- responses.py
+++ responses.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from app import app
 import requests
+import traceback
 
 url = app.config['QUOTEFAULT_ADDR'] + '/' + app.config['QUOTEFAULT_KEY']
 
@@ -11,16 +12,36 @@ def help_msg(command: str):
 			text = wrong + "Help for CSH Quotefault bot.\n"
 			+ "All commands are in the form `/quote command_name [data and arguements]`\n\n"
 			+ "`help` - displays this message\n"
-			+ "`random` - grabs a random quote and posts it to the current channel",
+			+ "`random` - grabs a random quote and posts it to the current channel.\n"
+			+ "Arguements:\n"
+			+ "\t`--submitter [username]` - limit search to a specific submitter by CSH username\n"
+			+ "\t`--date [date]` - limits search by date. I don't know what format the quotefault API uses nor do I know the function of this query, so... _good luck._",
 			response_type="ephemeral"
 			)
 
-def random():
-	response = requests.get(url + '/random').json()
-	return jsonify(
-			text = '> ' + response['quote'] + '\n-' + response['speaker'] + '\nSubmitted by: ' + response['submitter'],
-			response_type="in_channel"
-			)
-
-
-
+def random(request: str):
+	command = request.split(' ')
+	submitter = command[command.index('--submitter') + 1] if '--submitter' in command else ''
+	date = command[command.index('--date') + 1] if '--date' in command else ''
+	query = ''
+	if date or submitter:
+		query += '?'
+		if submitter:
+			query += 'submitter=' + submitter
+			if date:
+				query += '&'
+		if date:
+			query += 'date=' + date
+	try:
+		response = requests.get(url + '/random' + query).json()
+		app.logger.info(response) # Debug
+		return jsonify(
+				text = '> ' + response['quote'] + '\n-' + response['speaker'] + '\nSubmitted by: ' + response['submitter'],
+				response_type="in_channel"
+				)
+	except:
+		app.logger.warning('Query: "' + request + '", requests to API failed.\nError: ' + traceback.format_exc())
+		return jsonify(
+				text = 'Failed to query quotefault. Please try again. If that fails, message user:mom',
+				response_type = 'ephemeral'
+				)
