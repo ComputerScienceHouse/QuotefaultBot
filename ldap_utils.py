9e35acbc6f0118f32e2b8c5d847d62e7774a1c17
Max Meinhold <mxmeinhold@gmail.com>
2018-07-11 16:13:46 -0400

    Use display name in favour of cn + (uid)

--- ldap_utils.py
+++ ldap_utils.py
@@ -3,7 +3,7 @@ from app import _ldap
 def resolve_name(uid: str) -> str:
     try:
         member = _ldap.get_member(uid, uid=True)
-        return member.cn + ' (' + uid + ')'
+        return member.displayname
     except:
         return uid
 
