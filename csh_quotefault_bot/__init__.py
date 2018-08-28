869fbc953f6b64160253dc86f8cd870031cc4025
Max Meinhold <mxmeinhold@gmail.com>
2018-08-28 10:49:32 -0400

    Applies authentication to every route

--- csh_quotefault_bot/__init__.py
+++ csh_quotefault_bot/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, abort
+import requests
 import csh_ldap
 from csh_quotefault_bot import responses, ldap_utils
 
@@ -20,7 +21,7 @@ others = ['submit']
 
 _url = app.config['QUOTEFAULT_ADDR'] + '/' + app.config['QUOTEFAULT_KEY']
 
-responses.init(_url, multiples, app.config['OAUTH_TOKEN'])
+responses.init(_url, multiples)
 
 @app.route('/')
 def index():
@@ -36,11 +37,28 @@ def get_quote(): # pylint: disable=inconsistent-return-statements
     app.logger.info(request.form) # Debug
     if app.config['VERIFICATION_TOKEN'] == request.form['token']:
         command = request.form['text'].split(' ')[0]
+
+        # Authenticate.
+        addr = 'https://slack.com/api/users.profile.get?'
+        addr += 'token=' + app.config['OAUTH_TOKEN'] + '&user=' + request.form['user_id']
+        res = requests.get(addr)
+        print(res.json())
+        email = res.json()['profile']['email']
+        if '@csh.rit.edu' in email:
+            uid = email.split('@')[0]
+        else:
+            return '''Could not find your CSH username.
+Please set your email to your CSH email at https://cshrit.slack.com/account/settings#email'''
+        if ldap_utils.resolve_name(uid) == uid:
+            return '''Your email on Slack seems to be an alias.
+Please use your base email so I can verify your identity.
+You can set that at https://cshrit.slack.com/account/settings#email'''
+
         if command == 'help':
             return responses.help_msg('')
         if command in singles + multiples:
             return responses.respond(request.form['text'])
         if command == 'submit':
-            return responses.submission(request.form['text'], request.form['user_id'])
+            return responses.submission(request.form['text'], uid)
         return responses.help_msg(command)
     abort(401)
